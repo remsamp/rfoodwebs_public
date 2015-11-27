4933f4c5ac35e9b0847c9fc555cb2bdc997402cd
Rems <remi.vergnon@free.fr>
2015-11-27 15:17:56 +0000

    remove an old dummy file

--- rfoodwebs_R_package/Rfoodwebs/R/foodwebfunctions.R
+++
@@ -1,7 +0,0 @@
-#' calculates the mean of a vector while excluding all NAs from it (roxygen comment)
-#' @param x a numeric vector
-
-# this is where we will write the R code generating food webs and food webs metrics
-mean.r <- function(x){
-  mean(x, na.rm = T)
-}
